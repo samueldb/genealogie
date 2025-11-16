8cba1f47fae19fa63e53dcc7526a957926b00a30
samueldb <samueldb@users.noreply.github.com>
2025-11-16 14:35:58 +0000

    Deploy dev preview: 643195c02408f14138b7cd3f61840512960b3de2 643195c02408f14138b7cd3f61840512960b3de2

--- deschampsberger/utils/createRelative.js
+++
@@ -1,7 +0,0 @@
-function createNewRelative(d) {
-    // Your code here
-    console.log('createNewRelative function called : '+d);
-    alert('createNewRelative function called : '+d);
-}
-
-export default createNewRelative;
